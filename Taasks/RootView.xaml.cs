810a3ad8b57bf4b9ba8414149ba27f397b2b3773
John <63595764+objetdefunc@users.noreply.github.com>
2020-05-15 19:27:04 +0100

    Basic App - Now in .NET 4.8 WPF

--- Taasks/RootView.xaml.cs
+++ Taasks/RootView.xaml.cs
@@ -16,11 +16,11 @@ using System.Windows.Shapes;
 namespace Taasks
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for RootView.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class RootView : UserControl
     {
-        public MainWindow()
+        public RootView()
         {
             InitializeComponent();
         }
